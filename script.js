372885d278b84269a353cb2a607465b5c29bf98f
cat903 <75070039+cat903@users.noreply.github.com>
2022-11-21 02:37:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var code = "";
 var colorState = null;
 
 function updateColor() {
-
   var element = canvas.getActiveObject();
   if (element == undefined) {
     return;
@@ -14,13 +13,37 @@ function updateColor() {
 
   for (idx in objX) {
     if (objX[idx].id == element.id) {
-      element.set('fill', colorChooser.value)
+      element.set("fill", colorChooser.value);
     }
   }
 
   canvas.renderAll();
   canvas.requestRenderAll();
+}
 
+function updateColor_two() {
+  var element = canvas.getActiveObject();
+  if (element == undefined) {
+    return;
+  }
+  clOne = document.getElementById('color').value;
+  clTwo = document.getElementById('color_x').value;
+  for (idx in objX) {
+    if (objX[idx].id == element.id) {
+      element.set("fill",new fabric.Gradient({
+        type:'linear',
+        gradientUnits: 'percentage',
+        coords: {x1:0,y1:0,x2:1,y2:0},
+        colorStops:[
+          {offset:0,color:clOne},
+          {offset:1,color:clTwo}
+        ]
+      }));
+    }
+  }
+
+  canvas.renderAll();
+  canvas.requestRenderAll();
 }
 
 function addRect() {
@@ -34,6 +57,35 @@ function addRect() {
   });
   objX.push(rect);
   canvas.add(rect);
+
+  var isMousedown = false;
+  rect.on("mousedown", () => {
+    isMousedown = true;
+  });
+  rect.on("mouseup", () => {
+    isMousedown = false;
+  });
+  rect.on("mousemove", () => {
+    if (isMousedown) {
+      document.querySelector(
+        ".axis"
+      ).innerText = `trXY:(${rect.lineCoords.tr.x.toFixed(
+        2
+      )},${rect.lineCoords.tr.y.toFixed(
+        2
+      )})\ntlXY:(${rect.lineCoords.tl.x.toFixed(
+        2
+      )},${rect.lineCoords.tl.y.toFixed(
+        2
+      )})\nblXY:(${rect.lineCoords.bl.x.toFixed(
+        2
+      )},${rect.lineCoords.bl.y.toFixed(
+        2
+      )})\nbrXY:(${rect.lineCoords.br.x.toFixed(
+        2
+      )},${rect.lineCoords.br.y.toFixed(2)})\n\n`;
+    }
+  });
 }
 
 function addCircle() {
@@ -47,6 +99,40 @@ function addCircle() {
 
   objX.push(circle);
   canvas.add(circle);
+
+  var isMousedown = false;
+  circle.on("mousedown", () => {
+    isMousedown = true;
+  });
+  circle.on("mouseup", () => {
+    isMousedown = false;
+  });
+  circle.on("mousemove", () => {
+    if (isMousedown) {
+      var xr = parseFloat(circle.radius);
+      var yr = parseFloat(circle.radius);
+
+      if (circle.scaleX != undefined) {
+        xr *= parseFloat(circle.scaleX);
+      }
+      if (circle.scaleY != undefined) {
+        yr *= parseFloat(circle.scaleY);
+      }
+      var cx =
+        (parseFloat(circle.lineCoords.tr.x) +
+          parseFloat(circle.lineCoords.bl.x)) /
+        2;
+      var cy =
+        (parseFloat(circle.lineCoords.tr.y) +
+          parseFloat(circle.lineCoords.bl.y)) /
+        2;
+        document.querySelector(
+          ".axis"
+        ).innerText = `CX:${cx.toFixed(2)}\nCY:${cy.toFixed(2)}\nXR:${xr.toFixed(
+          2
+        )}\nYR:${yr.toFixed(2)}`     
+    }
+  });
 }
 
 function addTriangle() {
@@ -58,9 +144,38 @@ function addTriangle() {
     width: 100,
     height: 100,
   });
-  console.log(Triangle);
   objX.push(Triangle);
   canvas.add(Triangle);
+
+  var isMousedown = false;
+  Triangle.on("mousedown", () => {
+    isMousedown = true;
+  });
+  Triangle.on("mouseup", () => {
+    isMousedown = false;
+  });
+  Triangle.on("mousemove", () => {
+    if (isMousedown) {
+      var fx = Triangle.lineCoords.bl.x.toFixed(2);
+      var fy = Triangle.lineCoords.bl.y.toFixed(2);
+
+      var sx = Triangle.lineCoords.br.x.toFixed(2);
+      var sy = Triangle.lineCoords.br.y.toFixed(2);
+
+      var tx =
+        (parseFloat(Triangle.lineCoords.tr.x.toFixed(2)) +
+          parseFloat(Triangle.lineCoords.tl.x.toFixed(2))) /
+        2;
+      var ty =
+        (parseFloat(Triangle.lineCoords.tr.y.toFixed(2)) +
+          parseFloat(Triangle.lineCoords.tl.y.toFixed(2))) /
+        2;
+
+        document.querySelector(
+          ".axis"
+        ).innerText = `(fx,fy):(${fx},${fy})\n(sx,sy):(${sx},${sy})\n(tx,ty):(${tx.toFixed(2)},${ty})\n`
+    }
+  });
 }
 
 function removeObj() {
@@ -73,12 +188,15 @@ function removeObj() {
   canvas.remove(element);
   canvas.renderAll();
   canvas.requestRenderAll();
+  document.querySelector(
+    ".axis"
+  ).innerText = ''
 }
 
 function printPos() {
   code = "";
   colorState = null;
-  var elEx = undefined; 
+  var elEx = undefined;
   document.getElementById("gcode").innerText = "";
   console.log(objX);
   for (obj of objX) {
@@ -91,21 +209,32 @@ function printPos() {
       genTriangle(obj);
     }
   }
-  elEx ? document.getElementById("gcode").value = a + b + c + code + d : document.getElementById("gcode").value = a + c + code + d; 
+  elEx
+    ? (document.getElementById("gcode").value = a + b + c + code + d)
+    : (document.getElementById("gcode").value = a + c + code + d);
 }
 
 function genRect(rect) {
-  var color = rect.fill;
-  if (color != colorState) {
+  var color = document.getElementById('color').value;
+  color_two = document.getElementById('color_x').value;
+  if(color!==color_two){
+    code += `\t${genGlcolor(color_two)}`
+  }
+  else if (color != colorState) {
     colorState = color;
     code += genGlcolor(color);
   }
-
   code += "\tglBegin(GL_QUADS);\n";
-  code += `\t\tglVertex2f(${rect.lineCoords.tr.x}f,${rect.lineCoords.tr.y}f);\n`;
-  code += `\t\tglVertex2f(${rect.lineCoords.tl.x}f,${rect.lineCoords.tl.y}f);\n`;
-  code += `\t\tglVertex2f(${rect.lineCoords.bl.x}f,${rect.lineCoords.bl.y}f);\n`;
-  code += `\t\tglVertex2f(${rect.lineCoords.br.x}f,${rect.lineCoords.br.y}f);\n`;
+  code += `\t\tglVertex2f(${rect.lineCoords.tr.x.toFixed(2)}f,${rect.lineCoords.tr.y.toFixed(2)}f);\n`;
+  if(color!==color_two){
+    code += `\t\t${genGlcolor(color)}`
+  }
+  code += `\t\tglVertex2f(${rect.lineCoords.tl.x.toFixed(2)}f,${rect.lineCoords.tl.y.toFixed(2)}f);\n`;
+  code += `\t\tglVertex2f(${rect.lineCoords.bl.x.toFixed(2)}f,${rect.lineCoords.bl.y.toFixed(2)}f);\n`;
+  if(color!==color_two){
+    code += `\t\t${genGlcolor(color_two)}`
+  }
+  code += `\t\tglVertex2f(${rect.lineCoords.br.x.toFixed(2)}f,${rect.lineCoords.br.y.toFixed(2)}f);\n`;
   code += "\tglEnd();\n";
 }
 
@@ -146,8 +275,12 @@ function genCircle(circle) {
 }
 
 function genTriangle(triangle) {
-  var color = triangle.fill;
-  if (color != colorState) {
+  var color = document.getElementById('color').value;
+  color_two = document.getElementById('color_x').value;
+  if(color!==color_two){
+    code += `\t${genGlcolor(color_two)}`
+  }
+  else if (color != colorState) {
     colorState = color;
     code += genGlcolor(color);
   }
@@ -169,7 +302,10 @@ function genTriangle(triangle) {
   code += "\tglBegin(GL_TRIANGLES);\n";
   code += `\t\tglVertex2f(${fx}f,${fy}f);\n`;
   code += `\t\tglVertex2f(${sx}f,${sy}f);\n`;
-  code += `\t\tglVertex2f(${tx}f,${ty}f);\n`;
+  if(color!==color_two){
+    code += `\t\t${genGlcolor(color)}`
+  }
+  code += `\t\tglVertex2f(${tx.toFixed(2)}f,${ty.toFixed(2)}f);\n`;
   code += "\tglEnd();\n";
 }
 
